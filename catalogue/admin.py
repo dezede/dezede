29ad2540eda02fa0e47cf0adad4843e9880aadfc
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-01-31 23:09:40 +0100

    Remplacement des listes abusives par des tuples.

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -16,15 +16,15 @@ class SourceInline(StackedInline):
     classes = ('collapse closed',)
 
 class EtatAdmin(VersionAdmin):
-    exclude = ['slug']
+    exclude = ('slug')
 
 class NatureDeLieuAdmin(VersionAdmin):
-    exclude = ['slug']
+    exclude = ('slug')
 
 class LieuAdmin(VersionAdmin):
-    exclude = ['slug']
-    filter_horizontal = ['illustrations', 'documents']
-    inlines = [AncrageSpatioTemporelInline,]
+    exclude = ('slug')
+    filter_horizontal = ('illustrations', 'documents')
+    inlines = (AncrageSpatioTemporelInline,)
     fieldsets = (
         ('Champs courants', {
             'fields': ('nom', 'parent', 'nature', 'historique',),
@@ -36,12 +36,12 @@ class LieuAdmin(VersionAdmin):
     )
 
 class ProfessionAdmin(VersionAdmin):
-    exclude = ['slug']
+    exclude = ('slug')
 
 class AncrageSpatioTemporelAdmin(VersionAdmin):
     list_display = ('__unicode__', 'date', 'heure', 'lieu', 'date_approx',
         'heure_approx', 'lieu_approx', 'calc_date', 'calc_heure', 'calc_lieu',)
-    search_fields = ['lieu__nom', 'lieu__parent__nom',]
+    search_fields = ('lieu__nom', 'lieu__parent__nom',)
     fieldsets = (
         ('Champs courants', {
             'fields': ('date', 'heure', 'lieu',),
@@ -56,9 +56,9 @@ class IndividuAdmin(VersionAdmin):
     list_display = ('__unicode__', 'nom', 'nom_naissance', 'calc_prenoms',
         'pseudonyme', 'sexe', 'ancrage_naissance', 'ancrage_deces',
         'calc_professions')
-    exclude = ['slug']
-    filter_horizontal = ['prenoms', 'professions', 'parentes', 'illustrations',
-        'documents']
+    exclude = ('slug')
+    filter_horizontal = ('prenoms', 'professions', 'parentes', 'illustrations',
+        'documents')
     fieldsets = (
         ('Champs courants', {
             'fields': ('nom', 'nom_naissance', 'prenoms', 'pseudonyme',
@@ -73,21 +73,21 @@ class IndividuAdmin(VersionAdmin):
     )
 
 class EngagementAdmin(VersionAdmin):
-    filter_horizontal = ['individus']
+    filter_horizontal = ('individus')
 
 class PersonnelAdmin(VersionAdmin):
-    filter_horizontal = ['engagements']
+    filter_horizontal = ('engagements')
 
 class GenreDOeuvreAdmin(VersionAdmin):
-    exclude = ['slug']
+    exclude = ('slug')
 
 class OeuvreAdmin(VersionAdmin):
     list_display = ('titre', 'soustitre', 'genre', 'calc_caracteristiques',
         'calc_auteurs', 'ancrage_composition',)
-    exclude = ['slug']
+    exclude = ('slug')
     filter_horizontal = ['caracteristiques', 'pupitres', 'auteurs', 'parentes',
         'documents', 'illustrations']
-    inlines = [ElementDeProgrammeInline,]
+    inlines = (ElementDeProgrammeInline,)
     fieldsets = (
         ('Titre', {
             'fields': ('prefixe_titre', 'titre', 'liaison',
@@ -105,14 +105,14 @@ class OeuvreAdmin(VersionAdmin):
     )
 
 class AttributionDeRoleAdmin(VersionAdmin):
-    filter_horizontal = ['individus']
+    filter_horizontal = ('individus')
 
 class ElementDeProgrammeAdmin(VersionAdmin):
-    filter_horizontal = ['caracteristiques', 'distribution', 'illustrations',
-        'documents']
+    filter_horizontal = ('caracteristiques', 'distribution', 'illustrations',
+        'documents')
 
 class EvenementAdmin(VersionAdmin):
-    filter_horizontal = ['programme', 'documents', 'illustrations']
+    filter_horizontal = ('programme', 'documents', 'illustrations')
     fieldsets = (
         ('Champs courants', {
             'fields': ('ancrage_debut', 'ancrage_fin', 'relache',
@@ -125,11 +125,11 @@ class EvenementAdmin(VersionAdmin):
     )
 
 class TypeDeSourceAdmin(VersionAdmin):
-    exclude = ['slug']
-    inlines = [SourceInline,]
+    exclude = ('slug')
+    inlines = (SourceInline,)
 
 class SourceAdmin(VersionAdmin):
-    filter_horizontal = ['evenements', 'documents', 'illustrations']
+    filter_horizontal = ('evenements', 'documents', 'illustrations')
     fieldsets = (
         ('Champs courants', {
             'fields': ('nom', 'numero', 'date', 'page', 'type', 'contenu',
