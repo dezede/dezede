15a05644ef660e6a11dcf93224c82d8787d9d613
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-03-02 12:55:35 +0100

    Rajoute les TEMPLATE_CONTEXT_PROCESSORS manquants.
    Change-Id: Ic05c651a54771fcdadf8e1876322eadfea98d4c6

--- settings.py
+++ settings.py
@@ -124,8 +124,11 @@ INSTALLED_APPS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
     'django.core.context_processors.request',
     'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
 )
 
