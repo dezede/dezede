9a415935f37897077b0bc6251591c0b514bdb3b6
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-05-13 22:00:06 +0200

    Nettoyage de code des URLconfs.

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from django.conf.urls.defaults import *
-from django.utils.translation import ugettext as _
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from musicologie import settings
 from musicologie.catalogue.views import *
@@ -17,22 +16,37 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^lieux/$', LieuListView.as_view(), name='lieux'),
-    url(r'^lieux/id/(?P<pk>\d+)/$', LieuDetailView.as_view(), name='lieu_pk'), # FIXME: réserver 'id' lors de la validation
+    # FIXME: réserver 'id' lors de la validation
+    url(r'^lieux/id/(?P<pk>\d+)/$', LieuDetailView.as_view(), name='lieu_pk'),
     url(r'^lieux/(?P<slug>[-\w]+)/$', LieuDetailView.as_view(), name='lieu'),
     url(r'^individus/$', IndividuListView.as_view(), name='individus'),
-    url(r'^individus/id/(?P<pk>\d+)/$', IndividuDetailView.as_view(), name='individu_pk'), # FIXME: réserver 'id' lors de la validation
-    url(r'^individus/(?P<slug>[-\w]+)/$', IndividuDetailView.as_view(), name='individu'),
-    url(r'^oeuvres/id/(?P<pk>\d+)/$', OeuvreDetailView.as_view(), name='oeuvre_pk'), # FIXME: réserver 'id' lors de la validation
-    url(r'^oeuvres/(?P<slug>[-\w]+)/$', OeuvreDetailView.as_view(), name='oeuvre'),
-    url(r'''^evenements/id/(?P<pk>\d+)/$''', EvenementDetailView.as_view(), name='evenement_pk'), # FIXME: réserver 'id' lors de la validation
+    # FIXME: réserver 'id' lors de la validation
+    url(r'^individus/id/(?P<pk>\d+)/$', IndividuDetailView.as_view(),
+        name='individu_pk'),
+    url(r'^individus/(?P<slug>[-\w]+)/$', IndividuDetailView.as_view(),
+        name='individu'),
+    # FIXME: réserver 'id' lors de la validation
+    url(r'^oeuvres/id/(?P<pk>\d+)/$', OeuvreDetailView.as_view(),
+        name='oeuvre_pk'),
+    url(r'^oeuvres/(?P<slug>[-\w]+)/$', OeuvreDetailView.as_view(),
+        name='oeuvre'),
+    # FIXME: réserver 'id' lors de la validation
+    url(r'''^evenements/id/(?P<pk>\d+)/$''', EvenementDetailView.as_view(),
+        name='evenement_pk'),
     url(r'''^evenements'''
         r'''(?:\/(?P<lieu_slug>[-\w]+))?'''
         r'''(?:\/(?P<year>\d+))?'''
         r'''(?:\/(?P<month>\d+))?'''
-        r'''(?:\/(?P<day>\d+))?/$''', EvenementListView.as_view(), name='evenements'),
-    url(r'^sources/(?P<pk>\d+)/$', SourceDetailView.as_view(), name='source_pk'),
-    url(r'^saisie/source/$', 'musicologie.catalogue.views.saisie_source', name='saisie_sources'),
-    url(r'^saisie/source/(?P<source_id>\d+)/$', 'musicologie.catalogue.views.saisie_source', name='saisie_source'),
+        r'''(?:\/(?P<day>\d+))?/$''',
+        EvenementListView.as_view(),
+        name='evenements'),
+    url(r'^sources/(?P<pk>\d+)/$', SourceDetailView.as_view(),
+        name='source_pk'),
+    url(r'^saisie/source/$', 'musicologie.catalogue.views.saisie_source',
+        name='saisie_sources'),
+    url(r'^saisie/source/(?P<source_id>\d+)/$',
+        'musicologie.catalogue.views.saisie_source',
+        name='saisie_source'),
     (r'^tinymce/', include('tinymce.urls')),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/filebrowser/', include('filebrowser.urls')),
@@ -56,4 +70,3 @@ if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
         url(r'^rosetta/', include('rosetta.urls')),
     )
-
