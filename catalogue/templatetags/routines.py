b4dda8a0c350cb2fe4f4c42a6a53ced413873cbb
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-09-27 01:08:09 +0200

    Ajout d'un bouton de suppression au front-end admin.

--- catalogue/templatetags/routines.py
+++ catalogue/templatetags/routines.py
@@ -6,6 +6,14 @@ from django.template.loader import render_to_string
 register = Library()
 
 
+def build_permission(app_label, model_slug, action):
+    return '%s.%s_%s' % (app_label, model_slug, action)
+
+
+def build_admin_view_name(perm):
+    return 'admin:%s' % perm.replace('.', '_')
+
+
 @register.simple_tag(takes_context=True)
 def frontend_admin(context, object=None):
     if object is None:
@@ -13,13 +21,19 @@ def frontend_admin(context, object=None):
     Model = object.__class__
     app_label = Model._meta.app_label
     model_slug = Model.__name__.lower()
-    change_perm = '%s.%s_%s' % (app_label, model_slug, 'change')
-    has_change_permission = context['request'].user.has_perm(change_perm)
-    admin_change = 'admin:%s' % change_perm.replace('.', '_')
+    change_perm = build_permission(app_label, model_slug, 'change')
+    delete_perm = build_permission(app_label, model_slug, 'delete')
+    user = context['request'].user
+    has_change_perm = user.has_perm(change_perm)
+    has_delete_perm = user.has_perm(delete_perm)
+    admin_change = build_admin_view_name(change_perm)
+    admin_delete = build_admin_view_name(delete_perm)
     pk = object.pk
     c = {
-        'has_change_permission': has_change_permission,
+        'has_change_perm': has_change_perm,
+        'has_delete_perm': has_delete_perm,
         'admin_change': admin_change,
+        'admin_delete': admin_delete,
         'pk': pk,
     }
     return render_to_string('routines/front-end_admin.html', c)
