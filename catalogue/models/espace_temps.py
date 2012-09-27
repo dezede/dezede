c126b9edb1456d18527b45c5cb67924ceb8dcce1
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-09-27 17:40:18 +0200

    Corrige l'ordre des événements.

--- catalogue/models/espace_temps.py
+++ catalogue/models/espace_temps.py
@@ -202,7 +202,7 @@ class AncrageSpatioTemporel(CustomModel):
                                       'ancrages spatio-temporels', 1)
         verbose_name_plural = ungettext_lazy('ancrage spatio-temporel',
                                              'ancrages spatio-temporels', 2)
-        ordering = ['date', 'heure', 'lieu', 'date_approx',
+        ordering = ['date', 'heure', 'lieu', 'lieu__parent', 'date_approx',
                     'heure_approx', 'lieu_approx']
         app_label = 'catalogue'
 
