ec11921c63ce95559512205333b423cbdd444f0b
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-10-03 00:07:47 +0200

    Modifie le nom de la base de données.

--- dezede/settings.py
+++ dezede/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'musicologie',                      # Or path to database file if using sqlite3.
+        'NAME': 'dezede',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
