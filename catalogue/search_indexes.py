53c8f00a805c5ad2e1d8d7b2360a7a65da9171fd
Bertrand <bordage.bertrand@gmail.com>
2012-10-05 13:47:09 +0200

    Ajout de champs de suggestions pour Solr.

--- catalogue/search_indexes.py
+++ catalogue/search_indexes.py
@@ -4,6 +4,7 @@ from .models import *
 
 class OeuvreIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    suggestions = indexes.CharField()
 
     def get_model(self):
         return Oeuvre
@@ -11,12 +12,18 @@ class OeuvreIndex(indexes.RealTimeSearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        prepared_data = super(OeuvreIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 site.register(Oeuvre, OeuvreIndex)
 
 
 class SourceIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     date = indexes.DateField(model_attr='date')
+    suggestions = indexes.CharField()
 
     def get_model(self):
         return Source
@@ -24,11 +31,17 @@ class SourceIndex(indexes.RealTimeSearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        prepared_data = super(SourceIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 site.register(Source, SourceIndex)
 
 
 class IndividuIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    suggestions = indexes.CharField()
 
     def get_model(self):
         return Individu
@@ -36,11 +49,17 @@ class IndividuIndex(indexes.RealTimeSearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        prepared_data = super(IndividuIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 site.register(Individu, IndividuIndex)
 
 
 class LieuIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    suggestions = indexes.CharField()
 
     def get_model(self):
         return Lieu
@@ -48,11 +67,17 @@ class LieuIndex(indexes.RealTimeSearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        prepared_data = super(LieuIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 site.register(Lieu, LieuIndex)
 
 
 class EvenementIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    suggestions = indexes.CharField()
 
     def get_model(self):
         return Evenement
@@ -60,5 +85,10 @@ class EvenementIndex(indexes.RealTimeSearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        prepared_data = super(EvenementIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 site.register(Evenement, EvenementIndex)
 
