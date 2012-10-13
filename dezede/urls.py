1d999ca9bfb297cd5ad3b64a0a52d81bd5fa5f86
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-10-14 01:56:10 +0200

    Typo dans dezede/urs.py

--- dezede/urls.py
+++ dezede/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^recherche/', include('haystack.urls')),
     url(r'^comptes/', include('accounts.urls')),
-    url(r'^profils/', include('profiles.urls')
+    url(r'^profils/', include('profiles.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
