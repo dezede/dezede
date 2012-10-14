54d410afba2eba0372a81bb3c7729a2b36142dd4
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-10-14 03:39:55 +0200

    Ajoute un nom verbeux aux profils étudiants.

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,20 @@
+# coding: utf-8
+
 from django.contrib.auth.models import User
 from django.db.models import Model, OneToOneField, ForeignKey, permalink
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ungettext_lazy, ugettext_lazy as _
 
 
 class StudentProfile(Model):
     user = OneToOneField(User, related_name='student_profile')
     professor = ForeignKey(User, related_name='students',
                            verbose_name=_('professeur'))
+
+    class Meta:
+        verbose_name = ungettext_lazy(u'profil étudiant',
+                                      u'profils étudiants', 1)
+        verbose_name_plural = ungettext_lazy(u'profil étudiant',
+                                             u'profils étudiants', 2)
 
     def __unicode__(self):
         user = self.user
@@ -16,4 +24,4 @@ class StudentProfile(Model):
     @permalink
     def get_absolute_url(self):
         return ('profiles_profile_detail',
-                                          (), {'username': self.user.username})
+                (), {'username': self.user.username})
