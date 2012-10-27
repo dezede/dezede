87ca5ec4803672bc9de7a613713310efd81d3b4c
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-10-27 04:14:48 +0200

    Applique les cookies sur tout le site.

--- dezede/templates/base.js
+++ dezede/templates/base.js
@@ -1,6 +1,6 @@
 {# Gestion des tooltips #}
 if($.cookie("tooltips_placement") == null) {
-  $.cookie("tooltips_placement", "left");
+  $.cookie("tooltips_placement", "left", {path: '/'});
 }
 get_tooltips_options = function() {
   return {placement: $.cookie("tooltips_placement")};};
@@ -8,24 +8,25 @@ get_tooltips_options = function() {
 
 var tooltips = $('*[title], *[data-original-title]');
 tooltips_disable = function() {
-  $.cookie("tooltips_enabled", "false");
+  $.cookie("tooltips_enabled", "false", {path: '/'});
   tooltips.tooltip('destroy');
 };
 tooltips_enable = function() {
-  $.cookie("tooltips_enabled", "true");
+  $.cookie("tooltips_enabled", "true", {path: '/'});
   tooltips.tooltip(get_tooltips_options());
 };
 
 
 var tooltips_enabled = $.cookie("tooltips_enabled");
-if(tooltips_enabled == null || tooltips_enabled == "true") {
-  tooltips_enable();
+tooltips_enable();
+if(tooltips_enabled == "false") {
+  tooltips_disable();
 }
 
 
 tooltips_reload = function() {tooltips_disable(); tooltips_enable();};
 tooltips_placement = function(dir) {
-  $.cookie("tooltips_placement", dir);
+  $.cookie("tooltips_placement", dir, {path: '/'});
   tooltips_reload();
 };
 {# Fin de la gestion des tooltips #}
