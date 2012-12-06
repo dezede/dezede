6640d1b8ca6903ba979a3777557be111b12d0cf4
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-12-06 03:57:45 +0100

    Ajoute un test pour les événements.

--- catalogue/tests/models/__init__.py
+++ catalogue/tests/models/__init__.py
@@ -2,4 +2,5 @@ from .common import EtatTestCase
 from .espace_temps import LieuTestCase, SaisonTestCase
 from .individu import IndividuTestCase
 from .oeuvre import OeuvreTestCase
+from .evenement import EvenementTestCase
 from .source import SourceTestCase
---
+++ catalogue/tests/models/evenement.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from django.test import TestCase
+from ...models import *
+
+
+class EvenementTestCase(TestCase):
+    fixtures = ['catalogue/tests/models/fixtures/auth.json',
+                'catalogue/tests/models/fixtures/evenement.json']
+
+    def setUp(self):
+        self.nicolas = Evenement.objects.all()[0]
+
+    def testProgrammeRendering(self):
+        programme = self.nicolas.programme.all()
+        self.assertEqual(unicode(programme[0]),
+                         '« Avez-vous été bien sages les enfants ? »')
+        self.assertEqual(unicode(programme[1]),
+                         'Le père Fouettard, ou la correction méritée, '
+                         'mélodrame [première représentation]')
+        self.assertEqual(unicode(programme[2]),
+                         'Distribution de tatanées')
+        self.assertEqual(unicode(programme[3]),
+                         'L’arrivée du père Noël, ou Le Saint Nicolas vengeur')
+        self.assertEqual(unicode(programme[4]),
+                         'Distribution de pains d’épices')
