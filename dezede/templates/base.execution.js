6ffc192da553781b7c8ecc66145fb668142e809d
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-12-18 17:21:30 +0100

    Ajoute Google Analytics.

--- dezede/templates/base.execution.js
+++ dezede/templates/base.execution.js
@@ -5,3 +5,16 @@ tooltips_reload();
 {# Gestion des cadres d’alerte #}
 $('.alert').alert();
 {# Fin de la gestion des cadres d’alerte #}
+
+{% Google Analytics %}
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-37152824-1']);
+_gaq.push(['_setDomainName', 'dezede.org']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+{% Fin de Google Analytics %}
