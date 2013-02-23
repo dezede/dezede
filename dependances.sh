a551ec1cd1d1c91b39d5f7d7ccc66e35f927b552
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-02-23 19:01:44 +0100

    Corrige le script de dépendances.

--- dependances.sh
+++ dependances.sh
@@ -2,18 +2,13 @@
 
 apt-get install nano postgresql python2.7 python-psycopg2 python-pip python-docutils python-dev apache2 libapache2-mod-wsgi memcached
 
+# Pour satisfaire la construction de python-imaging (alias PIL)
+apt-get build-dep python-imaging
+
+# Liens symboliques pour que PIL trouve ses dépendances.
+# Sans cela, Django ne peut importer que des GIF et pas de JPEG, PNG, etc.
+ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
+ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
+ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
+
 pip install -r requirements.txt
-
-git clone git://github.com/mbi/django-rosetta.git
-cd django-rosetta
-git checkout e1c0fc1b18a9c406d60ea1103fadd25f21b1cef0
-python setup.py install
-cd ..
-rm -rf django-rosetta
-
-git clone git://github.com/jmacul2/django-rosetta-grappelli.git
-cd django-rosetta-grappelli
-git checkout 7e715c3de31b8db4396d331816d11c1bb5746020
-python setup.py install
-cd ..
-rm -rf django-rosetta-grappelli
