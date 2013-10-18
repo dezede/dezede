25bee9e4f3e35620a52ef692453c0a9b23589fba
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-10-18 14:08:59 +0200

    Corrige un test.

--- libretto/tests/models/evenement.py
+++ libretto/tests/models/evenement.py
@@ -37,7 +37,7 @@ class EvenementTestCase(CommonTestCase):
         self.assertEqual(
             smart_text(programme[5]),
             'Molière [auteur dram.], Le Tartuffe, ou l’Imposteur, '
-            'comédie en cinq actes et en vers. — Gassion (É.), '
+            'comédie en cinq actes et en vers. — Gassion (É.) '
             'dite La Môme Piaf [chanteuse]')
 
     def testRendering(self):
