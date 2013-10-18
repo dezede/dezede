815911ff34e062e72daec3cf254285d6632c7414
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-10-18 23:29:12 +0200

    Petites corrections javascript.

--- dezede/templates/base.definitions.js
+++ dezede/templates/base.definitions.js
@@ -1,30 +1,30 @@
 {# Gestion des tooltips #}
-get_tooltips_options = function() {
+function get_tooltips_options() {
   return {
     placement: 'top',
     container: 'body'
   };
-};
+}
 
-get_tooltips = function() {
+function get_tooltips() {
   return $('*[title], *[data-original-title]');
-};
-tooltips_disable = function() {
+}
+function tooltips_disable() {
   $.cookie("tooltips_enabled", "false", {path: '/'});
   get_tooltips().tooltip('destroy');
-};
-tooltips_enable = function() {
+}
+function tooltips_enable() {
   $.cookie("tooltips_enabled", "true", {path: '/'});
   get_tooltips().tooltip(get_tooltips_options());
-};
+}
 
-tooltips_reload = function() {
+function tooltips_reload() {
   var tooltips_enabled = $.cookie("tooltips_enabled");
   tooltips_enable();
   if(tooltips_enabled == "false") {
     tooltips_disable();
   }
-};
+}
 {# Fin de la gestion des tooltips #}
 
 {# Google Analytics #}
