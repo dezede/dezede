7715e9006e09770255a2cf3e87e5fec08799c2f1
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-11-11 04:31:32 +0100

    Ajoute un commentaire.

--- libretto/signals.py
+++ libretto/signals.py
@@ -8,6 +8,8 @@ from reversion.models import Version, Revision, post_revision_commit
 from .tasks import enqueue_with_stale_objects
 
 
+# FIXME: Revenir vite (plausiblement lors de la mise en place de Django 1.6)
+# à une version plus simple (comme 46df13bc9a7a09aa2ea1bcbaeb876d24b5f336ca).
 class CeleryAutoInvalidator(BaseSignalProcessor):
     def setup(self):
         post_revision_commit.connect(self.enqueue_save)
