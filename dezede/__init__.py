a7058352df6cd8c0e411df5e1b0948729f8ffe60
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-12-10 20:48:20 +0100

    Change le numéro de version pour 2.0.0.pre

--- dezede/__init__.py
+++ dezede/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import unicode_literals
 
 
-__version__ = 1, 8, 3
+__version__ = 2, 0, 0, 'pre'
 get_version = lambda: '.'.join(str(i) for i in __version__)
 __verbose_name__ = 'Dezède'
