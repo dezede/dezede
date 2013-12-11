0d38b086b7f383e687f9f5c7be5d8b5ed8ade663
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-12-11 15:23:46 +0100

    Retire les numéros de version de Django & Python + ajoute ISSN.

--- dezede/templatetags/dezede_extras.py
+++ dezede/templatetags/dezede_extras.py
@@ -4,28 +4,17 @@ from __future__ import unicode_literals
 from django import template
 from django.core.urlresolvers import reverse
 from django.utils.text import capfirst
-import sys
+import dezede
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def software_versions():
-    softwares = ('dezede', 'django', 'python')
-    out = []
-    for software in softwares:
-        if software == 'python':
-            name = software
-            version = '.'.join(str(i) for i in sys.version_info[:3])
-        else:
-            module = __import__(software)
-            name = getattr(module, '__verbose_name__', '') \
-                or getattr(module, '__name__', '') or software
-            version = module.get_version()
-        name = capfirst(name)
-        out.append('<span>%s\u00A0%s</span>' % (name, version))
-    return ' '.join(out)
+def dezede_version():
+    name = capfirst(dezede.__verbose_name__)
+    version = dezede.get_version()
+    return '%s\u00A0%s' % (name, version)
 
 
 @register.simple_tag(takes_context=True)
