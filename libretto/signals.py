0138eacf0d518b86e819a70000b7b527434a6b35
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-05-12 10:56:09 +0200

    Change les arguments passés à celery pour gérer la sérialisation JSON.

--- libretto/signals.py
+++ libretto/signals.py
@@ -3,13 +3,14 @@
 from __future__ import unicode_literals
 from celery_haystack.signals import CelerySignalProcessor
 from django.contrib.admin.models import LogEntry
+from django.contrib.sessions.models import Session
 from reversion.models import Version, Revision
 from .tasks import auto_invalidate
 
 
 class CeleryAutoInvalidator(CelerySignalProcessor):
     def enqueue(self, action, instance, sender, **kwargs):
-        if sender in (LogEntry, Revision, Version):
+        if sender in (LogEntry, Session, Revision, Version):
             return
 
-        auto_invalidate.delay(action, instance)
+        auto_invalidate.delay(action, instance.__class__, instance.pk)
