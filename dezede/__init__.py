45e1771f9b1b0399244c7fe3cfc7ea850be7eab7
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-05-28 11:35:46 +0200

    Met à jour le numéro de version.

--- dezede/__init__.py
+++ dezede/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from .celery import app as celery_app
 
 
-__version__ = 2, 0, 1
+__version__ = 2, 0, 1, 'post'
 get_version = lambda: '.'.join(str(i) for i in __version__)
 __verbose_name__ = 'Dezède'
