9833feb98dfc03357e2dd9222e62641105ebe0a3
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-06-06 11:51:41 +0200

    Version 2.0.3.

--- dezede/__init__.py
+++ dezede/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from .celery import app as celery_app
 
 
-__version__ = 2, 0, 2, 'post'
+__version__ = 2, 0, 3
 get_version = lambda: '.'.join(str(i) for i in __version__)
 __verbose_name__ = 'Dezède'
