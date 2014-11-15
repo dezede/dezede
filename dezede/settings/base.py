6fbe3671f45ea427fe17c55711489dcd17aa31a6
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-11-15 23:10:59 +0100

    Retire les renderers REST yaml et jsonp.

--- dezede/settings/base.py
+++ dezede/settings/base.py
@@ -335,8 +335,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.JSONPRenderer',
-        'rest_framework.renderers.YAMLRenderer',
         'rest_framework.renderers.XMLRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.DjangoFilterBackend'],
