bfeddc81bcbaadc2fff78fc5b12f2900e1423ee1
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-04-30 18:02:47 +0200

    Met à jour une migration.

--- libretto/migrations/0021_add_partie_type_oeuvre.py
+++ libretto/migrations/0021_add_partie_type_oeuvre.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='partie',
             name='oeuvre2',
-            field=models.ForeignKey(related_name='parties', verbose_name='work', blank=True, to='libretto.Oeuvre', null=True),
+            field=models.ForeignKey(related_name='parties', verbose_name='work', blank=True, to='libretto.Oeuvre', help_text='Ne remplir que pour les r\xf4les.', null=True),
             preserve_default=True,
         ),
         migrations.AddField(
