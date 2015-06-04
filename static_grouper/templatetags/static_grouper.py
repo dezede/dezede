d6972aeb83fe92171a391487631a688872ccb090
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-06-04 12:19:02 +0200

    Ajoute un argument nocompress à addstatic.

--- static_grouper/templatetags/static_grouper.py
+++ static_grouper/templatetags/static_grouper.py
@@ -13,8 +13,14 @@ CONTEXT_VARIABLE_NAME = 'static_grouper_dict'
 class AddStaticNode(Node):
     def __init__(self, parser, token):
         contents = token.split_contents()
-        if len(contents) != 2:
+        if len(contents) not in (2, 3):
             raise TemplateSyntaxError
+
+        if len(contents) == 3:
+            assert contents[2] == 'nocompress'
+            self.compress = False
+        else:
+            self.compress = True
 
         self.static_type = contents[1]
         self.nodelist = parser.parse(('endaddstatic',))
@@ -28,7 +34,8 @@ class AddStaticNode(Node):
             root_context[CONTEXT_VARIABLE_NAME] = \
                 static_grouper_dict = defaultdict(list)
         if output not in static_grouper_dict[self.static_type]:
-            static_grouper_dict[self.static_type].append(output)
+            static_grouper_dict[self.static_type].append(
+                (self.compress, output))
         return ''
 
 register.tag('addstatic', AddStaticNode)
@@ -49,14 +56,32 @@ class StaticListNode(Node):
 
         self.following_nodelist = parser.parse()
 
+    def groups_iterator(self, static_grouper_dict):
+        compressed_group = []
+        for compress, output in static_grouper_dict[self.static_type]:
+            if compress:
+                compressed_group.append(output)
+            else:
+                if compressed_group:
+                    yield True, ''.join(compressed_group)
+                    compressed_group = []
+                yield False, output
+        if compressed_group:
+            yield True, ''.join(compressed_group)
+
     def render(self, context):
         static_grouper_dict = context.get(CONTEXT_VARIABLE_NAME, defaultdict(list))
         following = self.following_nodelist.render(context)
-        inner = ''.join(static_grouper_dict[self.static_type])
-        if self.compress:
-            inner = CompressorNode(
-                nodelist=Template(inner).nodelist, kind=self.static_type,
-                mode='file').render(context=context)
+
+        inner = ''
+        for compress, output in self.groups_iterator(static_grouper_dict):
+            if compress and self.compress:
+                inner += CompressorNode(
+                    nodelist=Template(output).nodelist, kind=self.static_type,
+                    mode='file').render(context=context)
+            else:
+                inner += output
+
         return inner + following
 
 register.tag('static_list', StaticListNode)
