4d66f9db87a59c66a1517d06aca159dbf8b7cdc7
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-07-03 00:46:33 +0200

    Simplifie le passage du scheme https à Django.

--- dezede/wsgi.py
+++ dezede/wsgi.py
@@ -4,18 +4,14 @@ WSGI config for dezede project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from django.conf import settings
+
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dezede.settings.prod')
 
-
-def application(environ, start_response):
-    if not settings.DEBUG:
-        environ['wsgi.url_scheme'] = 'https'
-    return get_wsgi_application()(environ, start_response)
+application = get_wsgi_application()
