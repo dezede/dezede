b2727dc9958c70894ee7963c8d784589595a56dc
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-09-16 14:44:41 +0200

    Mises à jour diverses.

--- dezede/settings/base.py
+++ dezede/settings/base.py
@@ -146,8 +146,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
-    'allauth.account.context_processors.account',
-    'allauth.socialaccount.context_processors.socialaccount',
 )
 
 LOCALE_PATHS = (
