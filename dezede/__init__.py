d7551624436e8e5982ba191a83d11017fea9a9fb
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-02-09 02:31:33 +0100

    Dezède 2.8.0.

--- dezede/__init__.py
+++ dezede/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 
-__version__ = 2, 7, 7
+__version__ = 2, 8, 0
 get_version = lambda: '.'.join(str(i) for i in __version__)
 
 default_app_config = 'dezede.apps.DezedeConfig'
