2d14f185a1c605303128efba3c3ef6d3acd4c7f2
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-02-26 15:54:33 +0100

    Creates a migration to drop the NOT NULL constraint on HierarchicUser.last_login.

---
+++ accounts/migrations/0002_auto_20160226_1548.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import accounts.models
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='hierarchicuser',
+            managers=[
+                ('objects', accounts.models.HierarchicUserManager()),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='hierarchicuser',
+            name='email',
+            field=models.EmailField(blank=True, max_length=254, verbose_name='email address'),
+        ),
+        migrations.AlterField(
+            model_name='hierarchicuser',
+            name='groups',
+            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups'),
+        ),
+        migrations.AlterField(
+            model_name='hierarchicuser',
+            name='last_login',
+            field=models.DateTimeField(blank=True, null=True, verbose_name='last login'),
+        ),
+        migrations.AlterField(
+            model_name='hierarchicuser',
+            name='username',
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=30, unique=True, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.')], verbose_name='username'),
+        ),
+    ]
