1fbd44783627f76df1f60b77da06f2c1b724c8d9
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-06-16 13:47:48 +0200

    Fixes the CSV and JSON exports.

--- common/utils/export.py
+++ common/utils/export.py
@@ -162,6 +162,8 @@ def send_export(exporter_instance, extension, subject, filename, user_pk,
         get_failure_mail(subject, user).send()
         unlock_user(user)
         raise
+    if isinstance(file_content, tuple):
+        extension, file_content = file_content
     filename += '.' + extension
     get_success_mail(subject, user, filename, file_content,
                      exporter_instance.CONTENT_TYPES[extension]).send()
