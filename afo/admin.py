3c59ca3734d36746ae4ccbc491648d53b80181da
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-07-18 16:17:26 +0200

    Corrige la page des lieux de l’AFO.

--- afo/admin.py
+++ afo/admin.py
@@ -31,4 +31,4 @@ class LieuAFOAdmin(VersionAdmin, CommonAdmin):
 
     def get_queryset(self, request):
         qs = super(LieuAFOAdmin, self).get_queryset(request)
-        return qs.select_related('evenement', 'owner')
+        return qs.select_related('lieu', 'owner')
