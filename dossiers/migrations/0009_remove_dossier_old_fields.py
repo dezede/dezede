92674b46c9941443565561030e8bf839db6f3390
Cécile Hauchemaille <cecile.hauchemaille@gmail.com>
2023-10-27 23:17:18 +0200

    Removes backward migration.

--- dossiers/migrations/0009_remove_dossier_old_fields.py
+++ dossiers/migrations/0009_remove_dossier_old_fields.py
@@ -30,38 +30,6 @@ def migrate_data(apps, schema_editor):
             getattr(dossier_devenements, old_m2m_name[len('old_'):]).set(old_values)
 
 
-def migrate_data_backwards(apps, schema_editor):
-    Dossier = apps.get_model('dossiers', 'Dossier')
-    DossierDEvenements = apps.get_model('dossiers', 'DossierDEvenements')
-
-    old_fields = [
-        field.name for field in Dossier._meta.fields
-        if field.name.startswith('old_')
-    ]
-    old_m2m_names = [
-        m2m.name for m2m in Dossier._meta.many_to_many
-        if m2m.name.startswith('old_')
-    ]
-
-    for dossier_devenements in list(DossierDEvenements.objects.all()):
-        for old_field in old_fields:
-            setattr(
-                dossier_devenements,
-                old_field,
-                getattr(dossier_devenements, old_field[len('old_'):])
-            )
-
-        for old_m2m_name in old_m2m_names:
-            new_values = list(getattr(
-                dossier_devenements,
-                old_m2m_name[len('old_'):],
-            ).only('pk'))
-            getattr(dossier_devenements, old_m2m_name).set(new_values)
-
-        dossier_devenements.save()
-        dossier_devenements.delete(keep_parents=True)
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -70,7 +38,7 @@ class Migration(migrations.Migration):
 
     operations = [
         # Migrate data
-        migrations.RunPython(migrate_data, migrate_data_backwards),
+        migrations.RunPython(migrate_data),
 
         # Removes dossier old_* attributes
         migrations.RemoveField(
