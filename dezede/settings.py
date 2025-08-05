d7cf93a5ce6d9f616913614f72bbdd9f51d5f1a0
Cécile Hauchemaille <cecile.hauchemaille@gmail.com>
2025-08-05 20:27:14 +0200

    Updates debug toolbar settings.

--- dezede/settings.py
+++ dezede/settings.py
@@ -380,21 +380,23 @@ if DEBUG:
         'haystack_panel',
         'django_nose',
     ]
-    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+    # FIXME: remove setting when we remove Haystack, HaystackDebugPanel and TemplateTimings.
     DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.history.HistoryPanel',
         'debug_toolbar.panels.versions.VersionsPanel',
         'debug_toolbar.panels.timer.TimerPanel',
         'debug_toolbar.panels.settings.SettingsPanel',
         'debug_toolbar.panels.headers.HeadersPanel',
         'debug_toolbar.panels.request.RequestPanel',
         'debug_toolbar.panels.sql.SQLPanel',
-        # 'debug_toolbar.panels.templates.TemplatesPanel',
-        # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-        # 'debug_toolbar.panels.cache.CachePanel',
-        # 'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
         'debug_toolbar.panels.logging.LoggingPanel',
         'debug_toolbar.panels.redirects.RedirectsPanel',
-        # 'debug_toolbar.panels.profiling.ProfilingPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        # These third-party panels cannot be deactivated by default on the debug toolbar.
         # 'template_timings_panel.panels.TemplateTimings.TemplateTimings',
         'haystack_panel.panel.HaystackDebugPanel',
     ]
