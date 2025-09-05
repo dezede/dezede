99f7bb2d1e7c29e40d1137a8bbc84bf9ad380726
Bertrand Bordage <bordage.bertrand@gmail.com>
2025-09-05 23:25:15 +0200

    Adds a command to generate thumbnails.

---
+++ dezede/management/commands/generate_thumbnails.py
@@ -0,0 +1,40 @@
+from traceback import print_exc
+
+from django.apps import apps
+from django.core.management import BaseCommand
+from tqdm import tqdm
+
+from wagtail.images import get_image_model
+from wagtail.images.models import Rendition
+from wagtail.images.api.fields import ImageRenditionField
+
+
+class Command(BaseCommand):
+    help = (
+        "Generates all the missing thumbnails to speed up the initial load"
+        "of image-intensive pages."
+    )
+
+    def handle(self, *args, **options):
+        image_model = get_image_model()
+
+        for image in tqdm(image_model.objects.all(), desc='Admin thumbnails…'):
+            try:
+                image.get_rendition('max-165x165')
+            except (Rendition.DoesNotExist, OSError):
+                print_exc()
+
+        for model in apps.get_models():
+            for api_field in getattr(model, 'api_fields', []):
+                if not isinstance(api_field.serializer, ImageRenditionField):
+                    continue
+                for image in tqdm(
+                    image_model.objects.filter(
+                        pk__in=model.objects.values(api_field.serializer.source or api_field.name),
+                    ),
+                    desc=f'{model.__name__}.{api_field.name} frontend thumbnails…',
+                ):
+                    try:
+                        api_field.serializer.to_representation(image)
+                    except (image_model.DoesNotExist, OSError):
+                        print_exc()
