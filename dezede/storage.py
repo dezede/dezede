40a1d4f28221c8148970a21a9a5aa948d35b9ca1
Bertrand Bordage <bordage.bertrand@gmail.com>
2025-10-02 18:09:05 +0200

    Updates to Django 4.0.

--- dezede/storage.py
+++ dezede/storage.py
@@ -3,3 +3,10 @@ from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 class NonStrictManifestStaticFilesStorage(ManifestStaticFilesStorage):
     manifest_strict = False
+    # Prevents Django from trying to collect JS source map files.
+    # This is critical because we don't have them all, and Django fails to collectstatic
+    # when we miss one.
+    patterns = [
+        pattern for pattern in ManifestStaticFilesStorage.patterns
+        if pattern[0] != '*.js'
+    ]
