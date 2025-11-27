3aa1e580161af403372e2207ec57b2933af6dab0
Cécile Hauchemaille <cecile.hauchemaille@gmail.com>
2025-11-27 22:18:14 +0100

    Fixes  richtext to text conversion.

--- dezede/utils.py
+++ dezede/utils.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 def richtext_to_text(value: str):
     soup = BeautifulSoup(value, 'html.parser')
+    for tag in soup.select('a[linktype="note-reference"]'):
+        tag.clear()
     for no_space_tag in ['b', 'strong', 'i', 'em', 'a', 'sup', 'span.sc']:
         for tag in soup.select(no_space_tag):
             tag.unwrap()
