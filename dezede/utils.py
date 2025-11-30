0aaa74a6ba01203e6df3e728c56d6fb1d6400458
Cécile Hauchemaille <cecile.hauchemaille@gmail.com>
2025-12-01 00:32:11 +0100

    Optimizes richtext to text conversion.

--- dezede/utils.py
+++ dezede/utils.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def richtext_to_text(value: str):
     soup = BeautifulSoup(value, 'html.parser')
     for tag in soup.select('a[linktype="note-reference"]'):
-        tag.clear()
+        tag.extract()
     for no_space_tag in ['b', 'strong', 'i', 'em', 'a', 'sup', 'span.sc']:
         for tag in soup.select(no_space_tag):
             tag.unwrap()
