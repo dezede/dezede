d7c07e3ee9e7d8643506a13184b9020f1fdc9ce9
Cécile Hauchemaille <cecile.hauchemaille@gmail.com>
2026-05-07 19:01:16 +0200

    Removes 'Snippet' menu entry.

--- libretto/wagtail_hooks.py
+++ libretto/wagtail_hooks.py
@@ -6,6 +6,7 @@ from wagtail.permissions import ModelPermissionPolicy
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.chooser import SnippetChooserViewSet
 from wagtail.snippets.views.snippets import SnippetViewSet
+from wagtail.snippets.wagtail_hooks import SnippetsMenuItem
 from wagtail_linksnippet.richtext_utils import add_snippet_link_button
 
 from common.utils.text import capfirst
@@ -30,6 +31,12 @@ def register_icons(icons):
         'wagtailfontawesomesvg/solid/guitar.svg',
         'wagtailfontawesomesvg/solid/people-group.svg',
     ]
+
+
+# FIXME: replace with WAGTAILSNIPPETS_MENU_SHOW_ALL = False when upgrading to Wagtail 7.0.
+@hooks.register('construct_main_menu')
+def hide_snippets_menu_item(request, menu_items):
+    menu_items[:] = [item for item in menu_items if not isinstance(item, SnippetsMenuItem)]
 
 
 class AutoritePermissionPolicy(ModelPermissionPolicy):
