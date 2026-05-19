e18c1bf22200da3d9ee04eb8ad1d6c5fd55376c9
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-05-19 12:47:11 +0200

    Uses Wagtail search for global search.

--- dezede/views.py
+++ dezede/views.py
@@ -14,15 +14,16 @@ from django.utils.feedgenerator import DefaultFeed, Enclosure
 from django.utils.html import strip_tags
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView, TemplateView
-from haystack.views import SearchView
 from wagtail.models import Page
+from wagtail.search.backends import get_search_backend
+from wagtail.search.models import IndexEntry
 
 from common.utils.html import sanitize_html
 from dossiers.models import Dossier
 from libretto.models import (
     Oeuvre, Lieu, Individu, Source, Evenement, Ensemble, Profession, Partie,
 )
-from libretto.search_indexes import autocomplete_search, filter_sqs_published
+from libretto.search_indexes import autocomplete_search
 from typography.utils import replace
 from .models import Diapositive
 
@@ -59,36 +60,41 @@ class HomeView(ListView):
         return qs
 
 
-# TODO: Use the search engine filters to do this
-def clean_query(q):
-    return (q.lower().replace('(', '').replace(')', '').replace(',', '')
-            .replace('-', ' '))
+class SearchView(ListView):
+    model = IndexEntry
+    template_name = 'search/search.html'
 
+    def get_model_choices(self):
+        return [
+            {
+                'model': model,
+                'value': model._meta.label,
+                'label': model._meta.verbose_name_plural,
+            } for model in [
+                Dossier, Ensemble, Individu, Lieu, Oeuvre, Profession, Partie, Source, Evenement,
+            ]]
 
-class CustomSearchView(SearchView):
-    """
-    Custom SearchView to fix spelling suggestions.
-    """
+    def get_queryset(self):
+        self.q = replace(self.request.GET.get('q', ''))
+        self.selected_models = self.request.GET.getlist('models', [])
+        s = get_search_backend()
+        qs = IndexEntry.objects.all()
+        if self.selected_models:
+            qs = qs.filter(content_type__in=[
+                ContentType.objects.get_for_model(choice['model'])
+                for choice in self.get_model_choices()
+                if choice['value'] in self.selected_models
+            ])
+        return s.search(self.q, qs).get_queryset()
 
-    def build_form(self, form_kwargs=None):
-        self.request.GET = GET = self.request.GET.copy()
-        GET['q'] = replace(GET.get('q', ''))
-        return super(CustomSearchView, self).build_form(form_kwargs)
-
-    def extra_context(self):
-        context = {'suggestion': None}
-
-        if self.results.query.backend.include_spelling:
-            q = self.query or ''
-            suggestion = self.form.searchqueryset.spelling_suggestion(q) or ''
-            if clean_query(suggestion) != clean_query(q):
-                context['suggestion'] = suggestion
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            query=self.q,
+            model_choices=self.get_model_choices(),
+            selected_models=self.selected_models,
+        )
         return context
-
-    def get_results(self):
-        sqs = super(CustomSearchView, self).get_results()
-        return filter_sqs_published(sqs, self.request)
 
 
 def autocomplete(request):
