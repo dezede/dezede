b162327dd6132f1528433eea603ce1fced459ed8
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-07-14 02:53:11 +0200

    Updates AppRouterCacheProvider for Next 16.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { Bodoni_Moda, Bodoni_Moda_SC, Spectral, Spectral_SC } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import { NextIntlClientProvider } from "next-intl";
